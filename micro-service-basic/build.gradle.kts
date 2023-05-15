19f0a65d6e426cec84d0d34491ff4272a58bc124
eddie.lys <ysh.li@misumi.sh.cn>
2023-05-15 10:28:51 +0800

    update: 新增登录方式SMS和账号密码登录

--- micro-service-basic/build.gradle.kts
+++ micro-service-basic/build.gradle.kts
@@ -6,7 +6,6 @@ dependencies {
     api("com.olympus:olympus-artemis-starter")
 
     implementation("com.alibaba.fastjson2:fastjson2")
-    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-security")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
